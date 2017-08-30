281074ce491b05fc895be53b2d6620dea69f2a9b
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-08-30 08:31:30 -1000

    Add files via upload

---
+++ complexArrays.js
@@ -0,0 +1,24 @@
+var haylee = {
+	name:"Haylee",
+	age: 16,
+	grade:12,
+	friends: ["Jacob", "Macy", "Xiaolin"]
+};
+
+var jacob = {
+	name:"Jacob",
+	age: 17,
+	grade:12,
+	friends: ["Haylee", "Gianni", "Ian"]
+};
+
+var macy = {
+	name:"Macy",
+	age: 16,
+	grade:12,
+	friends: ["Dayton", "Menjam", "Anjelynn"]
+};
+
+var x = [haylee,jacob,macy];
+
+window.alert(x[1].friends[0]);
